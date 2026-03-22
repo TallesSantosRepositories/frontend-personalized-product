c8c1f25c6cb465f00f52e7020d21cfac3dc63de7
= <talles.saint@gmail.com>
2026-03-22 17:48:57 -0300

    Fixed path to system

--- src/shared/components/image-galery.tsx
+++ src/shared/components/image-galery.tsx
@@ -56,7 +56,7 @@ export default function ImageGallery() {
                         <img
                             key={index}
                             ref={isLast ? lastImageRef : null}
-                            src={`/public/${normalizeImagePath(image.fullPath)}`
+                            src={`/${normalizeImagePath(image.fullPath)}`
                             } // ajuste o campo conforme seu DTO
                             alt={image.description || image.fileName}
                             style={{ width: "100%", marginBottom: "8px", borderRadius: "8px" }}
--- src/shared/utils/string-formater.ts
+++ src/shared/utils/string-formater.ts
@@ -2,5 +2,9 @@ export function normalizeImagePath(fullPath: string): string {
 
     const index = fullPath.indexOf('public');
     if (index === -1) return fullPath;
-    return fullPath.substring(index + 'public'.length)
-}; 
+    return normalizePathToSystem(fullPath.substring(index + 'public'.length))
+};
+
+export function normalizePathToSystem(path: string): string {
+    return path.split('/').map(segment => encodeURIComponent(segment)).join('/');
+}
